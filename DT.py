532ace283b526ed57dfa0dfa97e84ba45406386c
ShiwenXu <vicky.xu071@gmail.com>
2019-12-02 20:16:38 -0500

    merge

--- DT.py
+++ DT.py
@@ -1,9 +1,16 @@
 from sklearn import tree
+import preprocess
 
-def run(X, y, X_validation, Y_validation, k_value):
+
+def run():
+    # X, y, X_validation, k_value
+    data_1000, lables_1000, data_600, labels_600, test_data, test_price = preprocess.obtain_result()
+    print(labels_600)
     clf = tree.DecisionTreeClassifier()
-    clf.fit(X, y)
-    y_predicted = clf.predict(X_validation)
-
+    clf.fit(data_600, labels_600)
+    y_predicted = clf.predict(test_data)
+    print(y_predicted)
     return y_predicted
 
+
+run()
--- KNN.py
+++ KNN.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 
 
-def run(X, y, X_validation, Y_validation, k_value):
+def run(X, y, X_validation, k_value):
     nbrs = neigh.KNeighborsClassifier(n_neighbors=k_value)
     nbrs.fit(X, y)
     y_predicted = nbrs.predict(X_validation)
