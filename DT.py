dfabee0c78c97bb8854526a79ed43f82fcfb0dd2
ShiwenXu <vicky.xu071@gmail.com>
2019-12-02 21:59:33 -0500

    updated k fold

--- DT.py
+++ DT.py
@@ -1,19 +1,21 @@
 from sklearn.tree import DecisionTreeClassifier
+import numpy as np
 
-
-def run(X, y, X_test, var, flag):
+def run(X, y, X_test, best_max_depth, best_min_samples_split, best_max_feature, flag):
+    print (np.shape(X))
+    print (np.shape(y))
     if flag == 1:
-        clf = DecisionTreeClassifier(max_depth=var)
+        clf = DecisionTreeClassifier(max_depth=best_max_depth)
         clf.fit(X, y)
         y_predicted = clf.predict(X_test)
         print(y_predicted)
     elif flag == 2:
-        clf = DecisionTreeClassifier(min_samples_split=var)
+        clf = DecisionTreeClassifier(min_samples_split=best_min_samples_split)
         clf.fit(X, y)
         y_predicted = clf.predict(X_test)
         print(y_predicted)
     elif flag == 3:
-        clf = DecisionTreeClassifier(max_features=var)
+        clf = DecisionTreeClassifier(max_features=best_max_feature)
         clf.fit(X, y)
         y_predicted = clf.predict(X_test)
         print(y_predicted)
